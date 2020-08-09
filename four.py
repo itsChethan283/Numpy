8e2220ed302c62670375d3dc46dacd9daaf72780
srichethan <63234676+srichethan@users.noreply.github.com>
2020-08-09 19:08:09 +0530

    My commit

---
+++ four.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 19:07:27 2020
+
+@author: Dell
+"""
+
+
---
+++ three.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+n = np.array([1, 2, 3])
+print(n)
+
+nm = np.array([(1, 2, 3), (4, 5, 6)])
+print(nm)
+
